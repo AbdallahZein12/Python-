b10c67ee83dcea4dd49cda6874c5ed538399dd4f
AbdallahZein12 <abdallahabdelmoneim7@gmail.com>
2023-08-03 12:19:24 -0400

    Added logging and json lessons

--- intermediate_python/intermediate_python_expanded.py
+++ intermediate_python/intermediate_python_expanded.py
@@ -365,11 +365,11 @@ print(my_string.startswith('H'))
 print(my_string.startswith('Hello'))
 print(my_string.endswith('World'))
 print(my_string.find('lo'))
-print(my_string.find('pp'))
+print(my_string.find('pp'))   #-1 if not found
 print(my_string.count('o'))
 print(my_string.count('p'))
 print(my_string.replace('World','Universe'))
-print(my_string.replace('Worrrrld','Universe'))
+print(my_string.replace('Worrrrld','Universe')) # if word not found nothing gets replaced
 my_string = "how are you doing"
 my_list = my_string.split()
 print(my_list)
@@ -424,8 +424,9 @@ print(my_string)
 print()
 print("-----COLLECTIONS-----")
 print()
-
 # Collections: Counter, namedtuple, OrderedDict, defaultdict, deque
+
+
 from collections import Counter, namedtuple, OrderedDict, defaultdict, deque
 
 a = "aaaaaabbbbccc"
@@ -697,12 +698,167 @@ print()
 
 import logging
 # 5 different log levels
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
+# logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
 
 
-logging.debug('This is a debug message')
-logging.info('This is an info message')
-logging.warning('This is a warning message')
-logging.error('This is an error message')
-logging.critical('This is a critical message')
+# logging.debug('This is a debug message')
+# logging.info('This is an info message')
+# logging.warning('This is a warning message')
+# logging.error('This is an error message')
+# logging.critical('This is a critical message')
 
+# import helper
+
+# logger = logging.getLogger(__name__)
+
+# # create handler
+# stream_h = logging.StreamHandler()
+# file_h = logging.FileHandler('file.log')
+
+# # level and the format
+
+# stream_h.setLevel(logging.WARNING)
+# file_h.setLevel(logging.ERROR)
+
+# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# stream_h.setFormatter(formatter)
+# file_h.setFormatter(formatter)
+
+# logger.addHandler(stream_h)
+# logger.addHandler(file_h)
+
+# logger.warning('This is a warning')
+# logger.error('This is an error')
+
+
+# import logging.config
+
+# logging.config.fileConfig('logging.conf')
+# logger = logging.getLogger('simpleExample')
+# logger.debug('This is a debug message')
+
+
+# try:
+#     a = [1,2,3]
+#     val = a[4]
+    
+# except IndexError as e:
+#     logging.error(e, exc_info=True)
+    
+# import traceback
+
+# try: 
+#     a = [1,2,3]
+#     val = a[4]
+# except:
+#     logging.error("The error is %s",traceback.format_exc())
+
+# from logging.handlers import RotatingFileHandler
+
+# logger = logging.getLogger(__name__)
+
+# logger.setLevel(logging.INFO)
+
+# # roll over after 2kb, and keep backup logs app.log.1, app.log.2, etc.
+
+# handler = RotatingFileHandler('app.log',maxBytes=2000, backupCount=5)
+# logger.addHandler(handler)
+
+# for _ in range(10000):
+#     logger.info('Hello, World!')
+
+
+# from logging.handlers import TimedRotatingFileHandler
+# import time
+# logger = logging.getLogger(__name__)
+# logger.setLevel(logging.INFO)
+
+# # s, m, h, d, midnight, w0
+# handler = TimedRotatingFileHandler('timed_test.log',when='s',interval=5 ,  backupCount=5)
+# logger.addHandler(handler)
+
+# for _ in range(6):
+#     logger.info('Hello, World!')
+#     time.sleep(5)
+
+# USE PYTHON JSON LOGGER
+
+print()
+print("-----JSON-----")
+print()
+
+import json
+
+""" 
+Python            JSON
+dict              object
+list, tuple       array
+str               string
+int, long, float  number
+True              true
+False             false
+None              null
+
+"""
+
+
+
+person = {"name":"John","age":25,"city":"NY","HasChildren":False,"titles":["engineer","programmer"]}
+
+personJSON = json.dumps(person, indent=4, sort_keys=True)
+print(personJSON)
+
+with open('person.json', 'w') as file:
+    json.dump(person,file, indent=4, sort_keys=True)
+    
+person = json.loads(personJSON)
+print(person)
+
+with open('person.json','r') as file:
+    person = json.load(file)
+    
+print(person)
+
+
+class User:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+        
+user = User('Max', 27)
+
+def encode_user(o):
+    if isinstance(o, User):
+        return {'name':o.name,'age':o.age, o.__class__.__name__: True}
+    else:
+        raise TypeError('Object of type User is not JSON serializable')
+    
+userJSON = json.dumps(user, default=encode_user)
+print(userJSON)
+
+from json import JSONEncoder
+class UserEncoder(JSONEncoder):
+    def default(self, o):
+        if isinstance(o, User):
+            return{'name':o.name,'age':o.age,o.__class__.__name__:True}
+        return JSONEncoder.default(self, o)
+    
+userJSON = json.dumps(user,cls=UserEncoder)
+print(userJSON)
+
+    
+userJSON = UserEncoder().encode(user)
+print(userJSON)
+
+
+
+user = json.loads(userJSON)
+print(user)
+
+def decode_user(dct):
+    if User.__name__ in dct:
+        return User(name=dct['name'], age=dct['age'])
+    return dct
+
+user = json.loads(userJSON, object_hook=decode_user)
+print(user.name)
