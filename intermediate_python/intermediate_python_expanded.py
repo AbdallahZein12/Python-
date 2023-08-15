05ab1169f212aca8b13a6245362915841f9533c2
AbdallahZein12 <abdallahabdelmoneim7@gmail.com>
2023-08-15 09:57:36 -0400

    Added decorators lesson

--- intermediate_python/intermediate_python_expanded.py
+++ intermediate_python/intermediate_python_expanded.py
@@ -958,3 +958,156 @@ print(arr)
 # note: numpy random generator uses a different number generator than the 
 # one from the Python standard library and also has a different seed function
 # np.random.seed(1)
+
+print()
+print("-----DECORATORS-----")
+print()
+
+# two types of decorators, function decorators and class decorators
+
+def start_end_decorator(func):
+    
+    def wrapper():
+        print("Start")
+        func()
+        print("End")
+    return wrapper
+
+def print_name():
+    print("Alex")
+
+print_name = start_end_decorator(print_name)
+
+print_name()
+
+
+def start_end_decorator(func):
+    
+    def wrapper():
+        print("Start")
+        func()
+        print("End")
+    return wrapper
+
+@start_end_decorator
+def print_name():
+    print("Alex")
+
+
+print_name()
+
+import functools
+
+def start_end_decorator(func):
+    
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        print("Start")
+        result = func(*args, **kwargs)
+        print("End")
+        return result
+    return wrapper
+
+@start_end_decorator
+def add5(x):
+    return x + 5
+
+res = add5(10)
+print(res)
+
+print(help(add5))
+print(add5.__name__)
+
+
+import functools
+
+def my_decorator(func):
+    
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        # Do something before your function
+        result = func(*args, **kwargs)
+        # Do something after your function
+        return result
+    return wrapper
+
+@my_decorator
+def add5(x):
+    return x + 5
+
+res = add5(10)
+print(res)
+
+
+
+def repeat(num_times):
+    def decorator_repeat(func):
+        @functools.wraps(func)
+        def wrapper(*args,**kwargs):
+            for _ in range(num_times):
+                result = func(*args,**kwargs)
+            return result
+        return wrapper
+    return decorator_repeat
+
+
+@repeat(num_times=4)
+def greet(name):
+    print(f'Hello {name}')
+    
+greet("Alex")
+
+
+def start_end_decorator(func):
+    
+    @functools.wraps(func)
+    def wrapper(*args,**kwargs):
+        print('Start')
+        result = func(*args, **kwargs)
+        print('End')
+        return result
+    return wrapper
+
+def debug(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        args_repr = [repr(a) for a in args]
+        kwargs_repr = [f"{k}={v!r}" for k,v in kwargs.items()]
+        signature = ", ".join(args_repr + kwargs_repr)
+        print(f"Calling {func.__name__}({signature})")
+        result = func(*args, **kwargs)
+        print(f"{func.__name__!r} returned {result!r}")
+        return result
+    return wrapper
+
+@debug
+@start_end_decorator
+def say_hello(name):
+    greeting = f'Hello {name}'
+    print(greeting)
+    return greeting
+
+say_hello('Bob')
+
+
+class CountCalls:
+    
+    def __init__(self, func):
+        self.func = func
+        self.num_calls = 0
+        
+    def __call__(self, *args, **kwargs):
+        self.num_calls += 1
+        print(f"This is called {self.num_calls} times")
+        return self.func(*args, **kwargs)
+
+
+
+
+
+@CountCalls
+def say_hello():
+    print('Hello')
+    
+say_hello()
+say_hello()
