12b80735376e43c3022b89cce102a6d43fd035fd
AbdallahZein12 <abdallahabdelmoneim7@gmail.com>
2023-09-28 15:27:45 -0400

    Added threading and queueing lesson

--- intermediate_python/intermediate_python_expanded.py
+++ intermediate_python/intermediate_python_expanded.py
@@ -1339,7 +1339,7 @@ if __name__ == "__main__":
     
 import time
 from threading import Thread, Lock
-from queue import Queue
+from queue import Queue   # Linear data structure that follows the FIFO principle (First in first out)
 
 database_value = 0
 
@@ -1372,3 +1372,51 @@ if __name__ == "__main__":
     print('end value: ', database_value)
         
     print('end main')
+
+
+
+from queue import Queue
+
+if __name__ == "__main__":
+    q = Queue()
+    
+    q.put(1)
+    q.put(2)
+    q.put(3)
+    
+    # 3 2 1 
+    first = q.get()
+    print(first)
+    
+    q.task_done()
+    q.join()
+    
+from threading import Thread, Lock, current_thread
+from queue import Queue
+import time
+
+def worker(q, lock):
+    while True:
+        value = q.get()
+        
+        # processing...
+        with lock:
+            print(f'in {current_thread().name} got {value}')
+        q.task_done()
+
+if __name__ == "__main__":
+    q = Queue()
+    
+    lock = Lock()
+    num_threads = 10
+    
+    for i in range(num_threads):
+        thread = Thread(target=worker, args=(q,lock))
+        thread.daemon = True
+        thread.start()
+        
+    for i in range(1,21):
+        q.put(i)
+        
+    q.join()
+        
