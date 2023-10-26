9176a860e86f29480bd92fe0e51bdf167d80b93e
AbdallahZein12 <abdallahabdelmoneim7@gmail.com>
2023-10-26 14:30:08 -0400

    Added

--- intermediate_python/intermediate_python_expanded.py
+++ intermediate_python/intermediate_python_expanded.py
@@ -1420,3 +1420,129 @@ if __name__ == "__main__":
         
     q.join()
         
+        
+print()
+print("-----MultiProcessing-----")
+print()
+
+from multiprocessing import Process, Value, Array
+import os
+
+def square_numbers():
+    for i in range(1000):
+        i * i 
+        
+        
+if __name__ == "__main__":
+    processes = []
+    num_processes = os.cpu_count()
+    # Num of cpus on the machine. Usually a good choice for the number of processes
+    
+    #create processes and assign a function for each process
+    for i in range(num_processes):
+        process = Process(target=square_numbers)
+        processes.append(process)
+        
+    for process in processes:
+        process.start()
+        
+    for process in processes:
+        process.join()
+        
+        
+        
+# Sharing data across different processes
+        
+from multiprocessing import Process, Value, Array, Lock
+import os
+import time
+
+def add_100(number, lock):
+    for i in range(100):
+        time.sleep(0.01)
+        with lock:
+            number.value += 1
+        
+
+if __name__ == "__main__":
+    
+    lock = Lock()
+    shared_number = Value('i', 0)
+    print("Number at beginning is ", shared_number.value)
+    
+    p1 = Process(target=add_100, args=(shared_number,lock))
+    p2 = Process(target=add_100, args=(shared_number,lock))
+    
+    p1.start()
+    p2.start()
+    
+    p1.join()
+    p2.join()
+    
+    print("Number at end is ", shared_number.value)
+    
+    
+    
+    
+from multiprocessing import Process, Value, Array, Lock
+import os
+import time
+
+def add_100(numbers, lock):
+    for i in range(100):
+        time.sleep(0.01)
+        for i in range(len(numbers)):
+            with lock:
+                numbers[i] += 1
+        
+
+if __name__ == "__main__":
+    
+    lock = Lock()
+    shared_array = Array('d', [0.0,100.0,200.0])
+    print("Array at beginning is ", shared_array[:])
+    
+    p1 = Process(target=add_100, args=(shared_array,lock))
+    p2 = Process(target=add_100, args=(shared_array,lock))
+    
+    p1.start()
+    p2.start()
+    
+    p1.join()
+    p2.join()
+    
+    print("Array at the end is ", shared_array[:])
+
+
+
+
+from multiprocessing import Process, Value, Array, Lock
+from multiprocessing import Queue
+import os
+import time
+
+def square(numbers,queue):
+    for i in numbers:
+        queue.put(i*i)
+        
+def make_negative(numbers, queue):
+    for i in numbers:
+        queue.put(-1*i)
+
+if __name__ == "__main__":
+    
+    numbers = range(1, 6)
+    
+    q = Queue()
+    
+    p1 = Process(target=square, args=(numbers,q))
+    p2 = Process(target=make_negative,args=(numbers,q))
+    
+    p1.start()
+    p2.start()
+    
+    p1.join()
+    p2.join()
+    
+    while not q.empty():
+        print(q.get())
